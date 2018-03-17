fa7e0217ffbd99f0089f9f770b88968a38c90d9a
hyuk22 <wjdgurdl272@gmail.com>
2018-03-17 16:57:27 +0900

    reupdate

--- chap4/postfix_expression/infix_to_postfix_conversion.h
+++
@@ -1,8 +0,0 @@
-#include "stackADT.h"
-#include <string.h>
-
-#define MAX_SIZE 100
-
-char *convert(char *infix);
-int read_line(FILE *fp, char str[], int size); 
-int get_priority(char op);
--- chap4/stack.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#define MAX_SIZE 100
-
-void push(char ch);
-char pop();
-int is_empty();
-int is_full();
